0bae91bb7b9dfb8fbfd42b2fc62487b37d4ed1fd
shjang7 <lucy.sh.jang@gmail.com>
2020-02-20 16:22:28 +0900

    Search#search_term

--- lib/search.rb
+++ lib/search.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class Search
-  def initialize
+  attr_reader :search_term
+  attr_accessor :uri
+
+  BASE_URL = 'https://api.collection.cooperhewitt.org/rest/'
+
+  def initialize(search_term)
+    @search_term = search_term
   end
 end
--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -1,6 +1,12 @@
 require File.expand_path('./lib/search.rb')
 
 describe Search do
-  context '' do
+  let(:test_term) { 'ceramic' }
+  let(:new_search) { Search.new(test_term) }
+
+  context 'Input search term' do
+    it 'reads a search term variable' do
+      expect(new_search.search_term).to eql(test_term)
+    end
   end
 end
