ac33254f8b5629db09c40fced59b0839507f2d7f
shjang7 <lucy.sh.jang@gmail.com>
2020-02-20 21:22:04 +0900

    Add search and parse html method

--- bin/main.rb
+++ bin/main.rb
@@ -4,4 +4,21 @@ require './lib/search.rb';
 require './lib/parse.rb';
 
 def main
+  if ARGV.empty?
+    abort('This program requires a search phrase as a commandline argument')
+  else
+    client = Search.new(ARGV)
+    begin
+      res = client.search
+    rescue RestClient::ExceptionWithResponse => e
+      puts ("#{e}")
+    end
+  end
+
+  unless res.nil?
+    searched_data = Parse.new(res)
+    print "#{searched_data[:title]} \n"
+    print "[LINK] #{searched_data[:link]} \n"
+    print "[DESCRIPTION] #{searched_data[:description]} \n"
+  end
 end
--- lib/parse.rb
+++ lib/parse.rb
@@ -1,6 +1,40 @@
 # frozen_string_literal: true
+require 'rubygems'
+require 'nokogiri'
 
 class Parse
-  def initialize
+  def initialize(data)
+    @data = data
+  end
+
+  def result(number)
+    data_list = begin
+      page.css('li h2 a')[0...number].map do |node|
+        { title: node.content, link: node.attr('href') }
+      end
+    end
+    desc = description(number)
+    data_list.each_index do |i|
+      next if i == 4 || i == 10 # Photo or Video search
+      data_list[i][:description] = desc[convert_index(i)]
+    end
+    data_list
+  end
+
+  private
+
+  def data_list(i)
+    j = i
+    j -= 1 if i > 4
+    j -= 1 if i > 10
+    j
+  end
+
+  def description(number)
+    number -= 1 if number >= 4
+    number -= 1 if number >= 10
+    page.css('li p')[0...number].map do |node|
+      node.content
+    end
   end
 end
