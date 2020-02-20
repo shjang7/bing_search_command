35f0eb4496738f5f234ce01360ffaa8910b554f4
shjang7 <lucy.sh.jang@gmail.com>
2020-02-20 20:47:31 +0900

    Update search site from api search to browser search with bing

--- lib/search.rb
+++ lib/search.rb
@@ -1,31 +1,21 @@
 # frozen_string_literal: true
 require 'rest-client';
-require 'json';
-require './config/app_environment_variables.rb';
 
 class Search
   attr_reader :search_term, :data
+  attr_accessor :uri
 
-  BASE_URL = 'https://api.collection.cooperhewitt.org/rest/'
-  S_METHOD = 'cooperhewitt.search.collection'
-  S_TYPE = 'medium'
-  S_ACCESS_TOKEN = ENV['COOPER_HEWITT_ACCESS_TOKEN']
+  BASE_URL = 'https://www.bing.com/'
 
   def initialize(search_term)
     @search_term = search_term
   end
 
   def data
-    JSON.parse RestClient.get(uri)
+    RestClient.get(uri)
   end
-
-  private
 
   def uri
-    @uri ||= "#{BASE_URL}#{search_query}"
-  end
-
-  def search_query
-    "?method=#{S_METHOD}&access_token=#{S_ACCESS_TOKEN}&#{S_TYPE}=#{search_term}&page=1&per_page=100"
+    @uri ||= "#{BASE_URL}search?q=#{search_term}"
   end
 end
--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -1,21 +1,18 @@
+require 'rest-client';
 require './lib/search.rb';
 
 describe Search do
+  let(:base_url) { 'https://www.bing.com/' }
   let(:test_term) { 'ceramic' }
   let(:new_search) { Search.new(test_term) }
-  let(:hash_keys) { new_search.data.keys }
 
   context '#initialize' do
     it { expect(new_search.search_term).to eql(test_term) }
+    it { expect(new_search.uri).to eql("#{base_url}search?q=#{test_term}") }
   end
 
   context '#data' do
-    it { expect(hash_keys).to include('objects') }
-    it { expect(hash_keys).to include('total') }
-    it { expect(hash_keys).to include('page') }
-    it { expect(hash_keys).to include('per_page') }
-    it { expect(hash_keys).to include('pages') }
-    it { expect(hash_keys).to include('stat') }
-    it { expect(hash_keys).to include('event_publishing_state') }
+    it { expect(new_search.data.code).to eq(200) }
+    it { expect(new_search.data).to be_kind_of(RestClient::Response) }
   end
 end
